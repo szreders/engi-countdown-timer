2df7cf4fbf3e5ef00da57f287d11c5c8d4f544b0
szreders <sebastian.szreder@enginiety.com>
2019-03-27 14:50:34 +0100

    IU-2018.3.5 <Seba@DESKTOP-9KFT2M2 Create markdown.xml, ide.general.xml, git.xml, project.default.xml, other.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, react component༏ext༏.component.js, filetypes.xml, androidEditors.xml, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, templates.xml, Default.xml, JavaScript.xml, laf.xml, jdk.table.xml, path.macros.xml, vcs.xml, editor.xml, React Fn Component.tsx, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/React Fn Component.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+    
+const Fn = () => ();
+    
+export default Fn;
---
+++ fileTemplates/react component༏ext༏.component.js
@@ -0,0 +1,10 @@
+// @flow
+import React, {Component} from 'react'
+
+export class ${StringUtils.removeAndHump(${NAME}, "-")}Component extends Component{ 
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
